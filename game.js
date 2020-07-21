28b8f7a25fbb07b7bfca5f27d90d9ae3976c5ea7
Sophia Li <sophiali@Sophias-MBP.lan>
2020-07-21 17:14:39 -0500

    day 2

--- game.js
+++ game.js
@@ -1,27 +1,26 @@
-const plants = [
-    {
-        name: "echinacea",
-        life: 0,
-        young_plant: 8,
-        buds: 9,
-        maturity: 15,
-        young_plant_img: href = "#", // img of young plant
-        buds_img: href = "#", // img of buds forming
-        maturity_img: href = "#", // img of blooms
-    },
+class Plants {
+    constructor (maturity){
+        this.life = 0;
+        this.maturity = maturity;
+        //this.img = /Users/sophiali/Documents/General_Assembly/projects/unit_1_project/Project_1_Grow_Your_Garden/Grow-Your-Garden-game/images;
+    }
 
-    {
-        name: "rose",
-        life: 0,
-        young_plant: 10,
-        buds: 18,
-        maturity: 20,
-        young_plant_img: href = "#", // img of young plant
-        buds_img: href = "#", // img of buds forming
-        maturity_img: href = "#", // img of blooms
-    },
+    grow () {
+        if (water) {
+            this.life += 1
+        } else if (weeding) {
+            this.life += 1
+        } 
+    }
 
-]
+}
+
+const plant1 = new Plants(15)
+const plant2 = new Plants(10)
+const plant3 = new Plants(12)
+const plant4 = new Plants(8)
+
+const allPlants = [plant1, plant2, plant3, plant4]
 
 const watering_cans = {
     basic: {
@@ -45,62 +44,85 @@ const plant_food = {
     },
 }
 
-const plant = () => {
 
+
+
+// const ask_to_plant = prompt ("Would you like to plant something?")
+
+
+const plant = () => {
+    const newPlant = document.createElement("img")
+    newPlant.setAttribute("class", "newPlant")
+    newPlant.setAttribute("src", "images/sprout.png")
+    document.getElementById("plants").appendChild(newPlant)
+    // if (Plants.life === Plants.maturity) {
+    //     newPlant.src = Plants.maturity_img
+    // }
 }
+
+
+
+let plant_life = 0
 
 const water = () => {
-    if (water) {
+    // if () {
         if (watering_cans.basic) {
-            plants.life += watering_cans.basic.feed_power
+            Plants.life += watering_cans.basic.feed_power
         }
         else if (watering_cans.better) {
-            plants.life += watering_cans.better.feed_power
+            Plants.life += watering_cans.better.feed_power
         }
-    }
+    // }
 }
 
-const weeds = document.getElementById("weed")
+const water_plants = document.getElementById("watering")
+water_plants.addEventListener("click", water)
+// water_plants.getAnimations()
 
 const weed_increase = () => {
-    const weeds_img = document.createElement("div")
-    weeds_img.setAttribute("src", "weeds.jpg") 
-    weeds.appendChild(weeds_img)
+    // if (plant_num > 1) {
+        let weed_count = 0
+        const weeds_img = document.createElement("img")
+        weeds_img.setAttribute("src", "images/mushroom.png")
+        weeds_img.classList.add("mushroom")
+        document.getElementById("weed").appendChild(weeds_img)
+        return weed_count += 1
+    // }
 }
 
-const weed = () => {
-    if (weed) { 
-        plants.life += 1
-        weeds.removeChild(weeds_img)
+const weeding = () => {
+    // if (weed_count > 0) {
+        const currentWeeds = document.querySelector(".mushroom")
+        document.getElementById("weed").removeChild(currentWeeds)
+        Plants.life += 1
+    // }
+}
+
+document.getElementById("planting").addEventListener("click", plant)
+weed_increase()
+document.getElementById("weeding").addEventListener("click", weeding)
+
+const feed = () => {
+    if (plant_food.basic) {
+        Plants.life += plant_food.basic.feed_power
+    } 
+    else if (plant_food.better) {
+        Plants.life += plant_food.better.feed_power
     }
-    else {
-        weed_increase()
-    }
 }
 
-const feed= () => {
-    if (feed) {
-        if (plant_food.basic) {
-            plants.life += plant_food.basic.feed_power
-        } 
-        else if (plant_food.better) {
-            plants.life += plant_food.better.feed_power
-        }
-        
-    }
-}
-
-let game_status = true
 
 
-while (game_status === true) {
+// let current_day = true
 
 
-    const water_plants = document.getElementById("watering")
-    water_plants.addEventListener("click", water())
+// while (current_day === true) {
+//     let planted = false
+// document.getElementById("planting").addEventListener("click", plant {
+//     planted = true
+// })
+//     
+    
 
-    const weeding_plants = document.getElementById("weeding")
-    weeding_plants.addEventListener("click", weed())
 
-}
-
+// }
