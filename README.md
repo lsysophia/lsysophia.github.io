c0b428fc61a536e630fb7b00130dd2228514f923
Sophia Li <sophiali@Sophias-MBP.lan>
2020-07-25 14:09:00 -0500

    pre-release

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Grow-Your-Garden-game
+
+[screenshot]
+
+## What is Grow Your Garden?
+
+It is a game that you have to weed as fast as you could while cultivate your garden plot to contain more flowers than weeds. If the plot has more weeds than flowers, the player lose. 
+
+## Technical Discussion
+
+HTML, CSS, JavaScript
+
+> List of the technologies you're using (HTML, CSS, JavaScript)
+
+### Notes on Game Structure
+
+
+
+> Code samples, description of challenges you overcame, etc.
+
+## The Making of [Your Game]
+
+> Any credits or notes you feel you should add
+
+## Opportunities for Future Growth
+
+> If you had more time to work on your game, what would you do?
--- game.js
+++ game.js
@@ -8,6 +8,17 @@ class Person {
             plant4: [],
         }
     }
+
+    keepScore (n) {
+        const totalPlant1 = this.portfolio.plant1.length * 1
+        const totalPlant2 = this.portfolio.plant2.length * 2
+        const totalPlant3 = this.portfolio.plant3.length * 3
+        const totalPlant4 = this.portfolio.plant4.length * 5
+
+        const plantScore = this.score + totalPlant1 + totalPlant2 + totalPlant3 + totalPlant4
+
+        return plantScore - n * 2
+    }
 }
 
 class Plants {
@@ -29,7 +40,7 @@ class Plant1 extends Plants {
 
     grow () {
         this.plantLife += 1
-        // console.log(this.plantLife)
+
         if (this.plantLife === 2) {
             document.getElementById(this.plantId).setAttribute("src", "images/flower_1.png")            
         }
@@ -86,6 +97,7 @@ const person = new Person()
 const allPlants = []
 let numberOfWeed = 0
 
+//creating garden plot with 100 individual spaces to be taken up by either flower or weed
 for (let i = 0; i < 100; i ++) {
     const newPlot = document.createElement("div")
     newPlot.setAttribute("class", "emptyDiv")
@@ -93,54 +105,59 @@ for (let i = 0; i < 100; i ++) {
     document.getElementById("garden").append(newPlot)
 }
 
-
+//watering function
 const water = () => {
     allPlants.forEach((plant) => {
         plant.grow()
     })
 }
- 
+
+//feeding function
 const feed = () => {
     allPlants.forEach((plant) => {
         plant.grow()
     })
 }
 
-
+//planting new plant function. new plant will get a random number as ID and will be added to player object portfolio
 const plantNew = (event) => {
-        const newPlantId = Math.random()
-        let plant = ""
-        if (event.currentTarget.id === "plant1") {
-            plant = new Plant1()
-            person.portfolio.plant1.push(plant)
-        } 
-        else if (event.currentTarget.id === "plant2") {
-            plant = new Plant2()
-            person.portfolio.plant2.push(plant)
-        } 
-        else if (event.currentTarget.id === "plant3") {
-            plant = new Plant3()
-            person.portfolio.plant3.push(plant)
-        } 
-        else {
-            plant = new Plant4()
-            person.portfolio.plant4.push(plant)
-        }
-        plant.plantId = newPlantId
-        allPlants.push(plant)
+    const newPlantId = Math.random()
+    let plant = ""
+    if (event.currentTarget.id === "plant1") {
+        plant = new Plant1()
+        person.portfolio.plant1.push(plant)
+    } 
+    else if (event.currentTarget.id === "plant2") {
+        plant = new Plant2()
+        person.portfolio.plant2.push(plant)
+    } 
+    else if (event.currentTarget.id === "plant3") {
+        plant = new Plant3()
+        person.portfolio.plant3.push(plant)
+    } 
+    else {
+        plant = new Plant4()
+        person.portfolio.plant4.push(plant)
+    }
+    plant.plantId = newPlantId
+    allPlants.push(plant)
 
-        const newPlant = document.createElement("img")
-        newPlant.setAttribute("id", newPlantId)
-        newPlant.setAttribute("class", "plants")
-        newPlant.setAttribute("src", "images/sprout.png")
-        
-        const emptyPlantDiv = document.querySelector(".emptyDiv")
-        emptyPlantDiv.insertAdjacentElement("beforebegin", newPlant)
-        emptyPlantDiv.remove()
+    const newPlant = document.createElement("img")
+    newPlant.setAttribute("id", newPlantId)
+    newPlant.setAttribute("class", "plants")
+    newPlant.setAttribute("src", "images/sprout.png")
+    
+    const emptyPlantDiv = document.querySelectorAll(".emptyDiv")
+    const randomEmptyDiv = emptyPlantDiv[Math.floor(Math.random() * emptyPlantDiv.length)]
+    if (randomEmptyDiv) {
+        randomEmptyDiv.insertAdjacentElement("beforebegin", newPlant)
+        randomEmptyDiv.remove()
 
-    activateNewPlant()
+        activateNewPlant()
+    }
 }
 
+//new plant will be activated when the old plant has been successfully grown
 const activateNewPlant = () => {
     person.portfolio.plant1.forEach((plant) => {
         if (plant.plantLife >= 2) {
@@ -159,6 +176,7 @@ const activateNewPlant = () => {
     })
 }
 
+//a random selection between the two images will be added as garden weed
 const weedGrowth = () => {
     const weeds_img = document.createElement("img")
     const weeds2_img = document.createElement("img")
@@ -168,11 +186,14 @@ const weedGrowth = () => {
     weeds2_img.classList.add("newWeed")
     const allWeeds = [weeds_img, weeds2_img]
 
-    const emptyWeedDiv = document.querySelector(".emptyDiv")
-    emptyWeedDiv.insertAdjacentElement("beforebegin", allWeeds[Math.floor(Math.random() * allWeeds.length)])
-    emptyWeedDiv.remove()
+    const emptyWeedDiv = document.querySelectorAll(".emptyDiv")
+    const randomEmptyDiv = emptyWeedDiv[Math.floor(Math.random() * emptyWeedDiv.length)]
+    if (randomEmptyDiv) {
+        randomEmptyDiv.insertAdjacentElement("beforebegin", allWeeds[Math.floor(Math.random() * allWeeds.length)])
+        randomEmptyDiv.remove()
 
-    return numberOfWeed += 1
+       return numberOfWeed += 1
+    }
 }
 
 const weedRemoval = () => {
@@ -181,7 +202,8 @@ const weedRemoval = () => {
     emptyDiv.setAttribute("class", "emptyDiv")
     if (weed) {
         weed.remove()
-        document.getElementById("garden").insertAdjacentElement("beforeend", emptyDiv)
+        document.getElementById("garden").append(emptyDiv)
+        return numberOfWeed -= 1
     }
 }
 
@@ -193,37 +215,31 @@ document.getElementById("weeding").addEventListener("click", weedRemoval)
 document.getElementById("watering").addEventListener("click", water)
 document.getElementById("feeding").addEventListener("click", feed)
 
+if (window.innerWidth < 700) {
+    document.getElementById("sun").style.display = "none"
+}
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 700) {
+        document.getElementById("sun").style.display = "none"
+    } else if (window.innerWidth >= 700) {
+        document.getElementById("sun").style.display = "inline"
+    }
+})
+
 const weedIncrease = setInterval(() => {
     weedGrowth()
+    const playerScore = person.keepScore(numberOfWeed)
+    document.getElementById("score").innerText = "Score: " + playerScore
     if (document.querySelector(".emptyDiv") === null) {
         if (allPlants.length > numberOfWeed) {
             alert ("Congrats! You have successfully made a garden!")
         } else if (allPlants.length < numberOfWeed) {
-            alert ("Congrats! You can collect your mushrooms for dinner!")
+            alert ("Your garden has been flooded by weeds. But You can collect your mushrooms for dinner!")
+        } else if (allPlants.lenth === numberOfWeed) {
+            alert ("You half-assed it! Try again?")
         }
+        document.querySelector(".mask").style.display = "flex"
         clearInterval(weedIncrease)
     }
-}, 2000)
-
-
-// const watering_cans = {
-//     basic: {
-//         feed_power: 1,
-//     },
-    
-//     better: {
-//         feed_power: 3,
-//         cost: 10,
-//     },
-// }
-
-// const plant_food = {
-//     basic: {
-//         feed_power: 1,
-//     },
-
-//     better:{
-//         feed_power: 2,
-//         cost: 15,
-//     },
-// }
+}, 1000)
